d0980791b2c8e33500b16f5b283b389a67e28bc2
Ignas Šimaitis <ignas.simaitis06@gmail.com>
2026-09-23 01:21:54 +0300

    feat: add index value to input

--- Hash/Hashing/HashFunc.cs
+++ Hash/Hashing/HashFunc.cs
@@ -10,10 +10,13 @@ public class HashFunc
         ulong b = 12483322617323990657UL;
         ulong c = 2658435062210208447UL;
         ulong d = 1587461109643488129UL;
-
-        foreach (byte value in data)
-        {
-            a = unchecked(a ^ value + b) ^ d;
+        
+        for (int i = 0; i < data.Length; i++)
+        {   
+            byte value = data[i];
+            ulong input = unchecked(value + (ulong)i);
+            
+            a = unchecked(a ^ input + b) ^ d;
             b = unchecked((b + c) ^ a) ^ d;
             c = unchecked(c ^ b + d) ^ a;
             d = unchecked((d ^ c) + a) ^ b;
